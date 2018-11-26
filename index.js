edb1fa163a56ee924d9d2addbcec7b75312f9658
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-26 03:26:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ function createPost(){
   let postTemplate = _.template(document.getElementById("post-template").innerHTML);
   let commentTemplate = _.tamplate(document.getElementById("comments-template").innerHTML);
 
+  // get blog values
+    var postTitle = document.getElementById("postTitle").value;
+    var postAuthor = document.getElementById("postAuthor").value;
+    var post = document.getElementById("postBody").value;
 
+    document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
 
-}
+    var blogSection = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
+    var commentsSection = commentsTemplate();
+    var postElement = document.getElementById("post");
+
+    postElement.innerHTML = blogSection;
+    postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
+  }
+
+  function postComment() {
+    var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+
+    var commentText = document.getElementById("commentText").value;
+    var commenterName = document.getElementById("commenter").value;
+
+    var commentsSection = document.getElementById("comments");
+    commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
+  }
