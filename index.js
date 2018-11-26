37a4b92520faa84808b81b17f5e89a012e7450c8
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-26 03:24:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function createPost(){
   let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   let postTemplate = _.template(document.getElementById("post-template").innerHTML);
-  let commentTemplate = document.getElementById("comments-template").innerHTML;
+  let commentTemplate = _.tamplate(document.getElementById("comments-template").innerHTML);
 
-  
+
 
 }
