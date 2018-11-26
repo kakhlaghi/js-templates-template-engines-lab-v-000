3537c70b9ee4194ad84b56e7fb504767601a2363
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-26 03:27:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function createPost(){
   let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   let postTemplate = _.template(document.getElementById("post-template").innerHTML);
-  let commentTemplate = _.tamplate(document.getElementById("comments-template").innerHTML);
+  let commentTemplate = _.template(document.getElementById("comments-template").innerHTML);
 
   // get blog values
     let postTitle = document.getElementById("postTitle").value;
