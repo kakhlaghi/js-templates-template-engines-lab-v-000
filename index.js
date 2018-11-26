9f0a4e16ec6d10409571b6eed83909fa9dd313c7
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-26 02:57:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function createPost(){
   var page-template = document.getElementById("page-template").value
   var post-template = document.getElementById("post-template").value
+
+  postNewPost()
 }
